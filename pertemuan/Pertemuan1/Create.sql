6a1fe25daf43c97683d53b75196a3745938f1d27
ritsadhil <haritsfadhil884@gmail.com>
2024-10-09 21:56:47 +0700

    latihan1

---
+++ pertemuan/Pertemuan1/Create.sql
@@ -0,0 +1,11 @@
+CREATE table mahasiswa (
+	Id INT primary key identity(1,1),
+	NPM INT,
+	Nama Varchar(50),
+	Kelas Varchar(3),
+	Jurusan Varchar(50),
+	Fakultas Varchar(50),
+
+
+
+)
---
+++ pertemuan/Pertemuan1/Insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO mahasiswa 
+VALUES
+(233040077, 'Harits Fadhil Kurnia', 'D', 'Teknik Informatika', 'Teknik'),
+(233040023, 'Nur', 'A', 'Teknik Sastra', 'Teknik'),
+(233030044, 'Roni', 'C', 'Sastra Mesin', 'Sastra'),
+(233030069, 'Hanif', 'B', 'Teknik Pangan', 'Teknik'),
+(233030055, 'Raihan', 'C', 'Teknik Hukum', 'Teknik');
+
+SELECT * FROM mahasiswa
+WHERE Jurusan = 'Teknik Informatika'
